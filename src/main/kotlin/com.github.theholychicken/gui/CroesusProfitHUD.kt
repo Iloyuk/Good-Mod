27b7e1a8c6810a747204c1eae6280799dd171e29
TheHolyChickn <theholychickn@gmail.com>
2025-02-12 12:30:49 -0500

    KEY RENDERING LOGIC!!!!

--- src/main/kotlin/com.github.theholychicken/gui/CroesusProfitHUD.kt
+++ src/main/kotlin/com.github.theholychicken/gui/CroesusProfitHUD.kt
@@ -60,12 +60,38 @@ object CroesusProfitHUD {
                 }
 
             // draw over most profit
+            /*
+             * TODO
+             * write an external method for different rendering types
+             * eg: key vs no key
+             * then do a when loop to select the proper one
+             */
             GL11.glDisable(GL11.GL_DEPTH_TEST)
             GuiScreen.drawRect(slotX, slotY, slotX+16, slotY+16, 0x8000FF00.toInt())
             GL11.glEnable(GL11.GL_DEPTH_TEST)
+            when {
+                CroesusChestParser.keyStatus -> {
+                    val slot2 = CroesusChestParser
+                        .runLoot
+                        .sortedByDescending { it.profit }
+                        .getOrNull(1)
+                        ?.index
+                    if (slot2 != null) {
+                        val slotX2 = slotPositions.get(slot2 - 9).first + guiLeft
+                        val slotY2 = slotPositions.get(slot2 - 9).second + guiTop
+                        renderProfitChestKey(slotX, slotY, slotX2, slotY2)
+                    }
+                    else {
+                        renderProfitNoChestKey(slotX, slotY)
+                    }
+                }
+                else -> {
+                    renderProfitNoChestKey(slotX, slotY)
+                }
+            }
         }
     }
-
+    /*
     fun sendLootChat() {
         CroesusChestParser.runLoot
             .filter { !it.purchased }
@@ -80,5 +106,18 @@ object CroesusProfitHUD {
                     )
                 )
             }
+    }
+     */
+
+    fun renderProfitChestKey(x: Int, y: Int, x2: Int, y2: Int) {
+        GL11.glDisable(GL11.GL_DEPTH_TEST)
+        GuiScreen.drawRect(x, y, x+16, y+16, 0x8000FF00.toInt())
+        GuiScreen.drawRect(x2, y2, x2+16, y2+16, 0x80E4D0AA.toInt())
+        GL11.glEnable(GL11.GL_DEPTH_TEST)
+    }
+    fun renderProfitNoChestKey(x: Int, y: Int) {
+        GL11.glDisable(GL11.GL_DEPTH_TEST)
+        GuiScreen.drawRect(x, y, x+16, y+16, 0x8000FF00.toInt())
+        GL11.glEnable(GL11.GL_DEPTH_TEST)
     }
 }
